fecb7e45625c9b64e01954dbc3d41faae2fa7e3b
Vladg24 <vg07102004@gmail.com>
2018-03-17 16:16:13 +0300

    Update Variable.java

--- src/sketchware/Logic/blocks/Variable.java
+++ src/sketchware/Logic/blocks/Variable.java
@@ -1,23 +1,62 @@
 package sketch.Logic.blocks;
 
 /**
- * This class works with different variables(String, Number, Bool)
- * @author Admin
+ * This class works with different variables(String, Number, Bool, Map)
+ * @author Vladg24
  */
 public class Variable {
     class Number {
+        
         public static void addVariable(){
-            
+            public String variable;
+        }
+        
+        public static void increaseOne(){
+            variable = variable + 1;
+        }
+        
+        public static void decreaseOne(){
+            variable = variable - 1;
         }
     }
     
     class String {
         public static void addVariable(){
-            
+            public String variable;
+        }
+        
+        public static void setVariableTo(String s){
+            variable = s;   
         }
     }
     
     class Boolean {
+        public static void addVariable(){
+            public boolean variable;
+        }
         
+        public static void setVariableTo(boolean b){
+            variable = b;   
+        }
+    }
+    
+    class Map {
+        public static void addVariable(){
+            String[] variable;
+        }
+        
+        public static void createNewMap(){
+            for(int i = 0;i<=variable.length();i++){
+                map[i] = null;   
+            }
+        }
+        
+        public static void insertKeyValue(String key, String value){
+           map[key] = value;
+        }
+        
+        public static String getKeyValue(String key){
+            return map[key];
+        }
     }
 }
